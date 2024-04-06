2303dbce8b86499ff8cb7b27b016a4d40a819715
Alexey@Work <ace@live.ru>
2024-04-06 11:30:52 +0300

    Added PromoCodesController

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Models/PromoCodeShortResponse.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Models/PromoCodeShortResponse.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
-using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
 
 namespace Otus.Teaching.PromoCodeFactory.WebHost.Models
 {
     public class PromoCodeShortResponse
     {
         public Guid Id { get; set; }
-        
+
         public string Code { get; set; }
 
         public string ServiceInfo { get; set; }
